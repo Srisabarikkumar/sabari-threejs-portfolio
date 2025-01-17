c52f155a3d0ff7ea9cb0d8e278417933291eb6cc
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2025-01-17 20:04:48 +0530

    new change

--- src/constants/index.js
+++ src/constants/index.js
@@ -26,7 +26,7 @@ export const navLinks = [
     }
 ];
 
-export const resume = "https://res.cloudinary.com/dfzlizgq1/image/upload/v1737113208/Sabari_Resume_fqsquzjw.pdf";
+export const resume = "https://docs.google.com/document/d/1XdyQ68ZOEMSzBlWupVsQtRMWARQQMhR6r-hK9c8GA_8/edit";
 
 export const myProjects = [
     {
